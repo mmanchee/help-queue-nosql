fb2a8164ad7c2edb9e51b3c09d74fc7aae8461cf
Mike Manchee <mikemanchee@gmail.com>
2020-12-06 19:27:25 -0800

    add beginings of authentication to signin

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 
+console.log("key ", process.env.REACT_APP_FIREBASE_API_KEY);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
