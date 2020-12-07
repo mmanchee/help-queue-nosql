a94d291a9f3e0cdeead62568efd046b94f12676c
Mike Manchee <mikemanchee@gmail.com>
2020-12-06 19:38:43 -0800

    add sign up in and out

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -12,7 +12,23 @@ function Signin(){
       console.log(error.message);
     });
   }
-
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+      console.log("Successfully signed in!");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+  function doSignOut() {
+    firebase.auth().signOut().then(function() {
+      console.log("Successfully signed out!");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
   return (
     <React.Fragment>
       <h1>Sign up</h1>
@@ -27,6 +43,20 @@ function Signin(){
           placeholder='Password' />
         <button type='submit'>Sign up</button>
       </form>
+      <h1>Sign In</h1>
+      <form onSubmit={doSignIn}>
+        <input
+          type='text'
+          name='signinEmail'
+          placeholder='email' />
+        <input
+          type='password'
+          name='signinPassword'
+          placeholder='Password' />
+        <button type='submit'>Sign in</button>
+      </form>
+      <h1>Sign Out</h1>
+      <button onClick={doSignOut}>Sign out</button>
     </React.Fragment>
   )
 }
